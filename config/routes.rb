7b90c7c9178a4b6a59efe960e15e44d4d7a02b27
James Peerless <jamespeerless@gmail.com>
2013-07-12 16:28:50 -0400

    rails_apps_composer: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 MaskmeMini::Application.routes.draw do
-  get "dashboard/index"
-  get "home/index"
+  get 'pricing', :controller => 'home', :action => 'pricing'
+  get 'portfolio', :controller => 'home', :action => 'portfolio'
+  get 'features', :controller => 'home', :action => 'features'
+  get 'faq', :controller => 'home', :action => 'faq'
+  get 'contact', :controller => 'home', :action => 'contact'
+  get 'about', :controller => 'home', :action => 'about'
+  get "dashboard", :controller => "dashboard", :action => "index"
+  root :to => "home#index"
   get "home/contact"
   get "home/faq"
   get "home/features"
